fae4390f10cf16e576387fc6908f4e0ba683d17f
Francois Botes <fbotes.work@gmail.com>
2024-02-06 08:48:45 +0200

    cleanup

--- CSVv2/Models/AppRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using CSVv2.Models.User;
-
-namespace CSVv2.Models
-{
-    public class AppRole
-    {
-        public ICollection<UserRole> UserRoles { get; set; }
-    }
-}
--- CSVv2/Models/User/UserRole.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CSVv2.Models.User
-{
-    public class UserRole
-    {
-        public AppUser User { get; set; }
-        public AppRole Role { get; set; }
-    }
-}
